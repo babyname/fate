d2cd6cb52412f7b351748b08015920d76baf83fc
godcong <jumbycc@163.com>
2017-09-29 20:18:46 +0800

    add cmd

--- model/charactor.go
+++ model/charactor.go
@@ -4,11 +4,13 @@ type Character struct {
 	Base
 	IsSur      bool
 	NameChar   string `gorm:"size:2"`
-	NameType   string `gorm:"size:2"`   //字属
-	NameRoot   string `gorm:"size:128"` //字根
-	Radical    string `gorm:"size:16"`  //部首
-	Strokes    int    //笔画数
-	FixStrokes int    //修正（繁体）笔画数
+	NameType   string `gorm:"size:2"`    //字属
+	NameRoot   string `gorm:"size:128"`  //字根
+	Radical    string `gorm:"size:16"`   //部首
+	Strokes    int    `gorm:"not null"`  //笔画数
+	TradName   string `gorm:"size:2"`    //繁体
+	FixStrokes int    `gorm:"not null"`  //修正（繁体）笔画数
+	Pinyin     string `gorm:"size:128"`  //拼音
 	Comment    string `gorm:"size:1024"` //备注
 }
 
@@ -21,9 +23,16 @@ func FindByNameChar(v interface{}, nc string) error {
 }
 
 func FindCharactersByStrokes(v interface{}, s []int) error {
-	return ORM().Where("strokes in (?)", s).Find(v).Error
+	return ORM().Where("fix_strokes in (?)", s).Find(v).Error
+}
+
+func FindCharactersByStroke(v interface{}, s int) error {
+	return ORM().Where("fix_strokes = ?", s).Find(v).Error
 }
 
 func FindCharactersWithFiveByStrokes(v interface{}, five string, s []int) error {
-	return ORM().Where("strokes in (?) and name_type = ? ", s, five).Find(v).Error
+	if five == "" {
+		return FindCharactersByStrokes(v, s)
+	}
+	return ORM().Where("fix_strokes in (?) and name_type = ? ", s, five).Find(v).Error
 }
--- model/five.go
+++ model/five.go
@@ -15,6 +15,10 @@ func FindFiveByMass(v interface{}, mass string) error {
 	return ORM().Where("mass = ?", mass).Find(v).Error
 }
 
-func FindFiveWithFirstByMass(v interface{}, fir, mass string) error {
-	return ORM().Where("first = ? and mass = ?", fir, mass).Find(v).Error
+func FindFiveWithFirstByMass(v interface{}, fir string, mass []string) error {
+	return ORM().Where("first = ? and mass in (?)", fir, mass).Find(v).Error
+}
+
+func (f *Five) StringFive() string {
+	return f.First + f.Second + f.Third
 }
