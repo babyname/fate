2d20568393e971d1140a37430aae4285659223a8
godcong <jumbycc@163.com>
2018-01-03 18:03:49 +0800

    v2 update

--- model/five_grid.go
+++ model/five_grid.go
@@ -1,5 +1,23 @@
 package model
 
-type WuGe struct {
+type FiveGrid struct {
+	SkyGrid    int //天格
+	LandGrid   int //地格
+	PersonGrid int //人格
+	OutGrid    int //外格
+	AllGrid    int //总格
+}
+
+// NewFiveGrid 新建一个五格属性
+// 五格是进行大衍计算的基础
+func NewFiveGrid(sg, lg, pg int) FiveGrid {
+	return FiveGrid{SkyGrid: sg, LandGrid: lg, PersonGrid: pg}
+}
+
+func outGrid() {
+
+}
+
+func allGrid() {
 
 }
