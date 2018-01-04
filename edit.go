1da6d5c38c182f5b71935c2200bfef535c47a964
godcong <jumbycc@163.com>
2018-01-04 11:26:09 +0800

    v2 update

--- edit.go
+++ edit.go
@@ -1,24 +1,17 @@
 package fate
 
-import (
-	"strings"
-
-	"log"
-
-	"github.com/godcong/fate/model"
-)
-
-func InsertChar(typ, val string, stk int) {
-	vs := strings.Split(val, "")
-	for _, v := range vs {
-		if strings.TrimSpace(v) != "" {
-			c := model.Character{
-				NameChar: v,
-				NameType: typ,
-				Strokes:  stk,
-			}
-			model.ORM().Create(&c)
-			log.Println(v)
-		}
-	}
-}
+//
+//func InsertChar(typ, val string, stk int) {
+//	vs := strings.Split(val, "")
+//	for _, v := range vs {
+//		if strings.TrimSpace(v) != "" {
+//			c := model.Character{
+//				NameChar: v,
+//				NameType: typ,
+//				Strokes:  stk,
+//			}
+//			model.ORM().Create(&c)
+//			log.Println(v)
+//		}
+//	}
+//}
--- edit_test.go
+++ edit_test.go
@@ -1,11 +1,5 @@
 package fate_test
 
-import (
-	"testing"
-
-	"github.com/godcong/fate"
-)
-
 var (
 	mu = []string{
 		"",
@@ -575,20 +569,21 @@ var (
 	}
 )
 
-func TestInsertChar(t *testing.T) {
-	for key, value := range mu {
-		fate.InsertChar("木", value, key+1)
-	}
-	for key, value := range jin {
-		fate.InsertChar("金", value, key+1)
-	}
-	for key, value := range shui {
-		fate.InsertChar("水", value, key+1)
-	}
-	for key, value := range huo {
-		fate.InsertChar("火", value, key+1)
-	}
-	for key, value := range tu {
-		fate.InsertChar("土", value, key+1)
-	}
-}
+//
+//func TestInsertChar(t *testing.T) {
+//	for key, value := range mu {
+//		fate.InsertChar("木", value, key+1)
+//	}
+//	for key, value := range jin {
+//		fate.InsertChar("金", value, key+1)
+//	}
+//	for key, value := range shui {
+//		fate.InsertChar("水", value, key+1)
+//	}
+//	for key, value := range huo {
+//		fate.InsertChar("火", value, key+1)
+//	}
+//	for key, value := range tu {
+//		fate.InsertChar("土", value, key+1)
+//	}
+//}
