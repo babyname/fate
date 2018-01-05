32a95615000490edf76fb65abc40104cb46cfd38
godcong <jumbycc@163.com>
2018-01-05 18:01:15 +0800

    v2 update

--- config/config.go
+++ config/config.go
@@ -2,61 +2,93 @@ package config
 
 import (
 	"flag"
-	"log"
 
 	"github.com/pelletier/go-toml"
 )
 
 type Config struct {
-	//Database []map[string]string `toml:"database"`
 	tree *toml.Tree
 }
 
 var config *Config
+var flags map[string]interface{}
+var l = *flag.String("l", "log.txt", "set log file path")
+var c = flag.String("c", "config.toml", "set toml config file path")
 
 func init() {
 	//loadConfig()
+	flags = make(map[string]interface{})
+	flags["l"] = l
+	flags["c"] = c
+	flags["d"] = *flag.Bool("d", false, "check debug output")
+}
+
+func GetFlag(name string) interface{} {
+	if v, b := flags[name]; b {
+		return v
+	}
+	return nil
+}
+
+func GetFlagString(name string) string {
+	if v, b := GetFlag(name).(string); b {
+		return v
+	}
+	return ""
+}
+
+func GetFlagBool(name string) bool {
+	if v, b := GetFlag(name).(bool); b {
+		return v
+	}
+	return false
 }
 
 func loadConfig() {
-	//bs, err := ioutil.ReadFile(*c)
-	//if err != nil {
-	//	log.Panic(err)
-	//}
-	var err error
-	c := flag.String("c", "config.toml", "load an toml config file")
 	config = new(Config)
-	config.tree, err = toml.LoadFile(*c)
-	if err != nil {
-		log.Panic(err)
-	}
+	config.tree, _ = toml.LoadFile(GetFlagString("c"))
 }
 
-func DefaultConfig() Config {
+func DefaultConfig() *Config {
 	if config == nil {
 		loadConfig()
 	}
-	return *config
+	return config
 }
 
-func (c Config) GetSub(name string) Config {
-	var b bool
-	c.tree, b = c.tree.Get(name).(*toml.Tree)
-	if b {
-		return c
+func (c *Config) GetSub(name string) Config {
+	var cfg Config
+	if v := c.Get(name); v != nil {
+		cfg.tree, _ = v.(*toml.Tree)
+	}
+	return cfg
+}
+
+func (c *Config) Get(name string) interface{} {
+	if c != nil && c.tree != nil {
+		return c.tree.Get(name)
 	}
 	return Config{}
 }
-func (c Config) GetString(name string) string {
-	v, b := c.tree.Get(name).(string)
+
+func (c *Config) GetString(name string) string {
+	v, b := c.Get(name).(string)
 	if b {
 		return v
 	}
 	return ""
 }
 
-func (c Config) GetStringWithDefault(name string, def string) string {
-	v, b := c.tree.Get(name).(string)
+func (c *Config) GetBool(name string) bool {
+	v, b := c.Get(name).(bool)
+	if b {
+		return v
+	}
+	return false
+}
+
+func (c *Config) GetStringWithDefault(name string, def string) string {
+	v, b := c.Get(name).(string)
 	if b {
 		return v
 	}
