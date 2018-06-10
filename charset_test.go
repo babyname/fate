5ba8159c96ed66be742319e1159587d0729469aa
jumbycc@163.com <godcong>
2018-06-10 22:41:05 +0800

    another vision v3 add like use god

--- charset_test.go
+++ charset_test.go
@@ -1,11 +1,23 @@
 package fate_test
 
 import (
+	"log"
 	"testing"
-
-	"github.com/godcong/fate"
 )
 
+type value struct {
+	s1 string
+}
+
+func testStruct() value {
+	v := value{
+		s1: "value",
+	}
+	log.Printf("%p  %p", &v, &v.s1)
+	return v
+}
+
 func TestInitAll(t *testing.T) {
-	fate.InitAll()
+	v1 := testStruct()
+	log.Printf("%p %p", &v1, &v1.s1)
 }
