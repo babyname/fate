5c978b9f57ffaeb5046e33bf80c26793164a9402
godcong <jumbycc@163.com>
2018-06-15 23:49:32 +0800

    change property

--- property.go
+++ property.go
@@ -1,55 +1,33 @@
 package fate
 
-type Property interface {
-	UseThreeTalent() bool
-	UseFiveGrid() bool
-	UseFivePhase() bool
-	UseZodiac() bool
+type Property struct {
+	UseThreeTalent bool //三才
+	UseFiveGrid    bool //五格
+	UseFivePhase   bool //字符五行
+	UseZodiac      bool //生肖喜忌
+	UseLikeUseGod  bool //喜用神
 }
 
-type stdProperty struct {
-	useThreeTalent bool //三才
-	useFiveGrid    bool //五格
-	useFivePhase   bool //字符五行
-	useZodiac      bool //生肖
-}
-
-var property Property
+var property *Property
 
 func init() {
 	property = DefaultProperty()
 }
 
-func (s *stdProperty) UseThreeTalent() bool {
-	return s.useThreeTalent
-}
-
-func (s *stdProperty) UseFiveGrid() bool {
-	return s.useFiveGrid
-}
-
-func (s *stdProperty) UseFivePhase() bool {
-	return s.useFivePhase
-}
-
-func (s *stdProperty) UseZodiac() bool {
-	return s.useZodiac
-}
-
-func DefaultProperty() Property {
-	property := &stdProperty{
-		useThreeTalent: false,
-		useFiveGrid:    false,
-		useFivePhase:   false,
-		useZodiac:      false,
+func DefaultProperty() *Property {
+	property := &Property{
+		UseThreeTalent: false,
+		UseFiveGrid:    false,
+		UseFivePhase:   false,
+		UseZodiac:      false,
 	}
 	return property
 }
 
-func SetProperty(p Property) {
+func SetProperty(p *Property) {
 	property = p
 }
 
-func GetProperty() Property {
+func GetProperty() *Property {
 	return property
 }
