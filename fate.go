75e4d3b86c889b590c6c86c58f07c2130ab1d795
godcong <jumbycc@163.com>
2018-06-24 23:13:36 +0800

    add eight character

--- fate.go
+++ fate.go
@@ -21,6 +21,7 @@ func (f *fate) SetLastName(lastName string) {
 	f.name = newName(lastName)
 }
 
+//SetLunarData 设定生日
 func (f *fate) SetLunarData(t time.Time) {
 	f.calendar = chronos.New(t)
 }
@@ -34,7 +35,10 @@ func (f *fate) MakeFirstName() *Name {
 	return f.name
 }
 
-//EightCharacter 计算生辰八字
+//EightCharacter 计算生辰八字(需先设定生日),按年柱,月柱,日柱,时柱 输出
 func (f *fate) EightCharacter() (string, string, string, string) {
-	return f.calendar.Lunar().EightCharacter()
+	if f.calendar != nil {
+		return f.calendar.Lunar().EightCharacter()
+	}
+	return "", "", "", ""
 }
