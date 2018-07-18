fa2c36b8d27f9b1f86a6dad45e19a5bf033f83b1
godcong <jumbycc@163.com>
2018-07-19 02:59:56 +0800

    change to mgo

--- stroke.go
+++ stroke.go
@@ -1,17 +1,18 @@
 package fate
 
 import (
-	"fmt"
-
 	"github.com/godcong/fate/mongo"
 )
 
-//CalculatorBestStroke 计算最佳笔画数
-func CalculatorBestStroke(character []*mongo.Character) error {
-	if len(character) > 2 {
-		return fmt.Errorf("CalculatorBestStroke: len overflow", )
-	}
-	return nil
+type Stroke struct {
+	LastStroke  []int
+	FirstStroke []int
 }
 
-
+//CalculatorBestStroke 计算最佳笔画数
+func CalculatorBestStroke(character []*mongo.Character) (error, []*Stroke) {
+	if len(character) > 2 {
+		//return fmt.Errorf("CalculatorBestStroke: len overflow", )
+	}
+	return nil, nil
+}
