5a5f1fa0985ce4e95e44852f74099c8a0c76cc39
godcong <jumbycc@163.com>
2018-07-18 16:52:51 +0800

    change mysql to mgo

---
+++ mongo/wuxing_test.go
@@ -0,0 +1,31 @@
+package mongo_test
+
+import (
+	"github.com/godcong/fate/model"
+	"github.com/godcong/fate/mongo"
+	"gopkg.in/mgo.v2"
+	"log"
+	"testing"
+)
+
+func TestWuXing(t *testing.T) {
+	mongo.Dial("localhost", &mgo.Credential{
+		Username: "root",
+		Password: "v2RgzSuIaBlx",
+	})
+
+	var by []*model.FivePhase
+	err := model.DB().Find(&by)
+	if err != nil {
+		panic(err)
+	}
+	for idx := range by {
+		b := by[idx]
+		dy1 := mongo.WuXing{
+			WuXing:  []string{b.First, b.Second, b.Third},
+			Fortune: b.Fortune,
+			Comment: b.Comment,
+		}
+		log.Println(mongo.InsertIfNotExist(mongo.C("wuxing"), &dy1))
+	}
+}
