50342f2b1601dadeff7df8549303f60199a34131
godcong <jumbycc@163.com>
2018-07-23 01:08:24 +0800

    add six martial

--- martial.go
+++ martial.go
@@ -1,10 +1,10 @@
 package fate
 
 type Martial struct {
-	BiHua     string `bson:"bi_hua"json:"bi_hua"`         //笔画
-	SanCai    string `bson:"san_cai"json:"san_cai"`       //三才
-	BaZi      string `bson:"ba_zi"json:"ba_zi"`           //八字
-	GuaXiang  string `bson:"gua_xiang"json:"gua_xiang"`   //卦象
-	TianYun   string `bson:"tian_yun"json:"tian_yun"`     //天运
-	ShengXiao string `bson:"sheng_xiao"json:"sheng_xiao"` //生肖
+	BiHua     bool `bson:"bi_hua"json:"bi_hua"`         //笔画
+	SanCai    bool `bson:"san_cai"json:"san_cai"`       //三才
+	BaZi      bool `bson:"ba_zi"json:"ba_zi"`           //八字
+	GuaXiang  bool `bson:"gua_xiang"json:"gua_xiang"`   //卦象
+	TianYun   bool `bson:"tian_yun"json:"tian_yun"`     //天运
+	ShengXiao bool `bson:"sheng_xiao"json:"sheng_xiao"` //生肖
 }
--- mongo/wuge.go
+++ mongo/wuge.go
@@ -1,8 +1,8 @@
-//三才五格
 package mongo
 
 import "gopkg.in/mgo.v2/bson"
 
+//WuGe
 type WuGe struct {
 	ID     bson.ObjectId `bson:"_id,omitempty"`
 	TianGe int           `bson:"tian_ge"`
@@ -12,18 +12,6 @@ type WuGe struct {
 	ZongGe int           `bson:"zong_ge"`
 }
 
-type SanCai struct {
-	ID             bson.ObjectId `bson:"_id,omitempty"`
-	TianCai        string        `bson:"tian_cai"`
-	TianCaiYinYang string        `bson:"tian_cai_yin_yang"`
-	RenCai         string        `bson:"ren_cai"`
-	RenCaiYinYang  string        `bson:"ren_cai_yin_yang"`
-	DiCai          string        `bson:"di_cai"`
-	DiCaiYingYang  string        `bson:"di_cai_ying_yang"`
-	Fortune        string        `bson:"fortune"` //吉凶
-	Comment        string        `bson:"comment"` //说明
-}
-
 func MakeWuGe(l1, l2, f1, f2 int) *WuGe {
 	return &WuGe{
 		TianGe: tianGe(l1, l2, f1, f2),
@@ -31,17 +19,6 @@ func MakeWuGe(l1, l2, f1, f2 int) *WuGe {
 		DiGe:   diGe(l1, l2, f1, f2),
 		WaiGe:  waiGe(l1, l2, f1, f2),
 		ZongGe: zongGe(l1, l2, f1, f2),
-	}
-}
-
-func MakeSanCai(wuGe *WuGe) *SanCai {
-	return &SanCai{
-		TianCai:        sanCaiAttr(wuGe.TianGe),
-		TianCaiYinYang: sanCaiYinYang(wuGe.TianGe),
-		RenCai:         sanCaiAttr(wuGe.RenGe),
-		RenCaiYinYang:  sanCaiYinYang(wuGe.RenGe),
-		DiCai:          sanCaiAttr(wuGe.DiGe),
-		DiCaiYingYang:  sanCaiYinYang(wuGe.DiGe),
 	}
 }
 
@@ -109,36 +86,4 @@ func waiGe(l1, l2, f1, f2 int) (n int) {
 //总格，姓加名的笔画总数  数理五行分类
 func zongGe(l1, l2, f1, f2 int) int {
 	return l1 + l2 + f1 + f2
-}
-
-// GenerateThreeTalent 计算字符的三才属性
-// 1-2木：1为阳木，2为阴木   3-4火：3为阳火，4为阴火   5-6土：5为阳土，6为阴土   7-8金：7为阳金，8为阴金   9-10水：9为阳水，10为阴水
-func sanCaiAttr(i int) string {
-	var attr string
-	switch i % 10 {
-	case 1, 2:
-		attr = "木"
-	case 3, 4:
-		attr = "火"
-	case 5, 6:
-		attr = "土"
-	case 7, 8:
-		attr = "金"
-	case 9, 0:
-		fallthrough
-	default:
-		attr = "水"
-	}
-
-	return attr
-}
-
-func sanCaiYinYang(i int) string {
-	var yy string
-	if i%2 == 1 {
-		yy = "阳"
-	} else {
-		yy = "阴"
-	}
-	return yy
 }
