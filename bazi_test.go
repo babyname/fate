e5d93359592285b4b7aeeb913871720c5bd0c5f5
godcong <jumbycc@163.com>
2018-08-09 17:24:17 +0800

    add generate bazi

--- bazi_test.go
+++ bazi_test.go
@@ -9,5 +9,5 @@ import (
 func TestPoint(t *testing.T) {
 	t1 := chronos.New("1985/09/10 06:00")
 	log.Println(t1.Lunar().EightCharacter())
-	log.Printf("%+v", Point(t1))
+	log.Printf("%+v", NewBazi(t1))
 }
