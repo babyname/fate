8b6ff60486f894dccc551b18afa4df85830beb11
godcong <jumbycc@163.com>
2018-08-11 02:27:03 +0800

    add xiyong

--- bazi_test.go
+++ bazi_test.go
@@ -1,7 +1,9 @@
-package fate
+package fate_test
 
 import (
 	"github.com/godcong/chronos"
+	"github.com/godcong/fate"
+
 	"log"
 	"testing"
 )
@@ -9,5 +11,5 @@ import (
 func TestPoint(t *testing.T) {
 	t1 := chronos.New("1985/09/10 06:00")
 	log.Println(t1.Lunar().EightCharacter())
-	log.Printf("%+v", NewBazi(t1))
+	log.Printf("%+v", fate.NewBazi(t1).XiYong())
 }
