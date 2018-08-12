f0a427b96b3e30b4794e7794dbd68814259beb9b
godcong <jumbycc@163.com>
2018-08-13 00:32:49 +0800

    add wuxing

--- mongo/wuxing.go
+++ mongo/wuxing.go
@@ -1,6 +1,9 @@
 package mongo
 
 import (
+	"log"
+	"strings"
+
 	"github.com/globalsign/mgo/bson"
 )
 
@@ -10,4 +13,29 @@ type WuXing struct {
 	WuXing  []string      `bson:"wu_xing"`
 	Fortune string        `bson:"fortune"`
 	Comment string        `bson:"comment"`
+}
+
+var wuxing map[string]*WuXing
+
+func GetWuXing() map[string]*WuXing {
+	return getWuXing()
+}
+
+func getWuXing() map[string]*WuXing {
+	var wx WuXing
+	wuxing = make(map[string]*WuXing)
+	iter := C("wuxing").Find(nil).Iter()
+	for iter.Next(&wx) {
+		mwx := WuXing{
+			ID:      wx.ID,
+			WuXing:  wx.WuXing,
+			Fortune: wx.Fortune,
+			Comment: wx.Comment,
+		}
+
+		key := strings.Join(mwx.WuXing, "")
+		log.Println(mwx)
+		wuxing[key] = &mwx
+	}
+	return wuxing
 }
