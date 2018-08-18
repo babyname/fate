5dbbb5540921606f1f3fc98cc48c6ffe2c9d5e14
godcong <jumbycc@163.com>
2018-08-18 17:59:10 +0800

    fix zhouyi

--- mongo/guaxiang.go
+++ mongo/guaxiang.go
@@ -1,5 +1,50 @@
 package mongo
 
-type GuaXiang struct {
+import (
+	"strings"
 
+	"github.com/globalsign/mgo/bson"
+)
+
+type GuaXiang struct {
+	ID       bson.ObjectId `bson:"_id,omitempty"`
+	ShangGua string        `bson:"shang_gua"`
+	ShangShu string        `bson:"shang_shu"`
+	XiaGua   string        `bson:"xia_gua"`
+	XiaShu   string        `bson:"xia_shu"`
+	GuaXiang string        `bson:"gua_xiang"`
+	GuaMing  string        `bson:"gua_ming"`
+	GuaYi    string        `bson:"gua_yi"`
+	GuaYun   string        `bson:"gua_yun"`
+	XiangYue string        `bson:"xiang_yue"`
+	FuHao    string        `bson:"fu_hao"`
+}
+
+var gua map[string]*GuaXiang
+
+func GetGuaXiang() map[string]*GuaXiang {
+	return getGuaXiang()
+}
+func getGuaXiang() map[string]*GuaXiang {
+	if gua == nil {
+		gua = make(map[string]*GuaXiang)
+		var zy GuaXiang
+		iter := C("zhouyi").Find(nil).Iter()
+		for iter.Next(&zy) {
+			mzy := GuaXiang{
+				ID:       zy.ID,
+				ShangGua: zy.ShangGua,
+				XiaGua:   zy.XiaGua,
+				GuaXiang: zy.GuaXiang,
+				GuaMing:  zy.GuaMing,
+				GuaYi:    zy.GuaYi,
+				GuaYun:   zy.GuaYun,
+				XiangYue: zy.XiangYue,
+			}
+
+			key := strings.Join([]string{zy.ShangGua, zy.XiaGua}, "")
+			gua[key] = &mzy
+		}
+	}
+	return gua
 }
--- zhouyi.go
+++ zhouyi.go
@@ -1,6 +1,8 @@
 package fate
 
 import (
+	"strings"
+
 	"github.com/godcong/fate/mongo"
 )
 
@@ -11,45 +13,93 @@ const (
 )
 
 const (
-	ShangQian = 0x00
-	ShangDui  = 0x01
-	ShangLi   = 0x02
-	ShangZhen = 0x03
-	ShangXun  = 0x04
-	ShangKan  = 0x05
-	ShangGen  = 0x06
-	ShangKun  = 0x07
-	XiaQian   = 0x00
-	XiaDui    = 0x10
-	XiaLi     = 0x20
-	XiaZhen   = 0x30
-	XiaXun    = 0x40
-	XiaKan    = 0x50
-	XiaGen    = 0x60
-	XiaKun    = 0x70
+	GuaXiangQian = 0x00
+	GuaXiangDui  = 0x01
+	GuaXiangLi   = 0x02
+	GuaXiangZhen = 0x03
+	GuaXiangXun  = 0x04
+	GuaXiangKan  = 0x05
+	GuaXiangGen  = 0x06
+	GuaXiangKun  = 0x07
+	//ShangQian = 0x00
+	//ShangDui  = 0x01
+	//ShangLi   = 0x02
+	//ShangZhen = 0x03
+	//ShangXun  = 0x04
+	//ShangKan  = 0x05
+	//ShangGen  = 0x06
+	//ShangKun  = 0x07
+	//XiaQian   = 0x00
+	//XiaDui    = 0x10
+	//XiaLi     = 0x20
+	//XiaZhen   = 0x30
+	//XiaXun    = 0x40
+	//XiaKan    = 0x50
+	//XiaGen    = 0x60
+	//XiaKun    = 0x70
 )
 
+var gua = []string{
+	GuaXiangKun:  "坤",
+	GuaXiangGen:  "艮",
+	GuaXiangKan:  "坎",
+	GuaXiangXun:  "巽",
+	GuaXiangZhen: "震",
+	GuaXiangLi:   "离",
+	GuaXiangDui:  "兑",
+	GuaXiangQian: "乾",
+}
+
 type ZhouYi struct {
-	gua [3]mongo.GuaXiang
+	gua [3]*mongo.GuaXiang
 }
 
 //QiGua 起卦
 func QiGua(name *Name) *ZhouYi {
 	x := CountStroke(name.lastChar...)
 	m := CountStroke(name.firstChar...)
-	getGua(x)
-	getGua(m)
+	b := CountStroke(append(name.lastChar, name.firstChar...)...)
+	ben := benGua(x, m)
+	bian := bianGua(ben, b)
+	//hu := huGua(ben)
+	return &ZhouYi{
+		gua: [3]*mongo.GuaXiang{
+			BenGua:  ben,
+			BianGua: bian,
+		},
+	}
+
 	return nil
 }
 
-func getGua(i int) {
+func (y *ZhouYi) Set(idx int, xiang *mongo.GuaXiang) {
+	y.gua[idx] = xiang
+}
+
+func getGua(i int) string {
+	if i = i % 8; i != 0 {
+		return gua[i-1]
+	}
+	return gua[7]
+}
+
+func getZhou(i int) int {
 	panic("")
 }
 
-func ShangGua(){
+func benGua(x, m int) *mongo.GuaXiang {
+	bg := strings.Join([]string{getGua(x), getGua(m)}, "")
+	gx := mongo.GetGuaXiang()
+	if v, b := gx[bg]; b {
+		return v
+	}
+	return nil
+}
+
+func bianGua(ben *mongo.GuaXiang, bian int) *mongo.GuaXiang {
 	panic("")
 }
 
-func XiaGua()  {
+func huGua(ben *mongo.GuaXiang, ) {
 	panic("")
 }
