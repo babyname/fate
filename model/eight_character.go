e7ffff17a7ca18a8ab0e76ce5d0721191425bc28
godcong <jumbycc@163.com>
2018-08-26 06:07:59 +0800

    mysql to mongo

--- model/eight_character.go
+++
@@ -1,4 +0,0 @@
-package model
-
-type EightCharacter interface {
-}
--- model/name.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type Name struct {
-	Base   `xorm:"extends"`
-	Last1  string
-	Last2  string
-	First1 string
-	First2 string
-}
