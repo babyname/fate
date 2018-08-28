26d83bccd757fec175bc44238f5f3de3006a4b4f
godcong <jumbycc@163.com>
2018-08-28 15:54:56 +0800

    fix shengxiao

--- mongo/shengxiao.go
+++ mongo/shengxiao.go
@@ -1,6 +1,35 @@
 package mongo
 
+import (
+	"github.com/globalsign/mgo/bson"
+)
+
 //ShengXiao 生肖喜忌
 type ShengXiao struct {
+	ID        bson.ObjectId `bson:"_id,omitempty"` //id
 	Character string
+	XiShu     bool
+	XiNiu     bool
+	XiHu      bool
+	XiTu      bool
+	XiLong    bool
+	XiShe     bool
+	XiMa      bool
+	XiYang    bool
+	XiHou     bool
+	XiJi      bool
+	XiGou     bool
+	XiZhu     bool
+	JiShu     bool
+	JiNiu     bool
+	JiHu      bool
+	JiTu      bool
+	JiLong    bool
+	JiShe     bool
+	JiMa      bool
+	JiYang    bool
+	JiHou     bool
+	JiJi      bool
+	JiGou     bool
+	JiZhu     bool
 }
