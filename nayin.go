ce06535048ad5c910c15c7e22cda2269894e9e95
godcong <jumbycc@163.com>
2018-09-05 15:16:19 +0800

    update comment

--- nayin.go
+++ nayin.go
@@ -1,5 +1,15 @@
 package fate
 
+import "github.com/godcong/chronos"
+
 //NaYin 纳音
 type NaYin struct {
+	calendar *chronos.Calendar
+}
+
+//NewNaYin 创建纳音
+func NewNaYin(calendar *chronos.Calendar) *NaYin {
+	return &NaYin{
+		calendar: calendar,
+	}
 }
