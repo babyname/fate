05adc900f40690cd31adf7294c4e5270b845eb1e
godcong <jumbycc@163.com>
2019-06-16 14:34:32 +0800

    update fate

--- wuxing.go
+++ wuxing.go
@@ -1,8 +1,15 @@
 package fate
 
+type Luck string
+
 //WuXing 五行：five elements of metal,wood,water,fire and earth
 type WuXing struct {
 	WuXing  string `json:"wu_xing"`
-	Luck    string `json:"luck"`
+	Luck    Luck   `json:"luck"`
 	Comment string `json:"comment"`
+}
+
+//FindWuXing find a wuxing
+func FindWuXing(wx string) *WuXing {
+	return nil
 }
