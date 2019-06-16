ee95b0a5731ad049c984503a1fc7cbf536942f7b
godcong <jumbycc@163.com>
2019-06-16 14:30:49 +0800

    update fate

--- wuxing.go
+++ wuxing.go
@@ -2,6 +2,7 @@ package fate
 
 //WuXing 五行：five elements of metal,wood,water,fire and earth
 type WuXing struct {
-	WuXing string `json:"wu_xing"`
-	Luck   string `json:"luck"`
+	WuXing  string `json:"wu_xing"`
+	Luck    string `json:"luck"`
+	Comment string `json:"comment"`
 }
