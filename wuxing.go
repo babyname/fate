b930ccbdfe075725e737b4b9a455db395477a4c9
godcong <jumbycc@163.com>
2019-06-17 13:40:37 +0800

    update fate

--- wuxing.go
+++ wuxing.go
@@ -1,6 +1,12 @@
 package fate
 
-type Luck string
+type Luck int
+
+var luckPoint = []string{"大凶", "凶", "凶多于吉", "吉凶参半", "吉多于凶", "吉", "大吉"}
+
+func (l *Luck) Point() int {
+	return int(*l) + 1
+}
 
 //WuXing 五行：five elements of metal,wood,water,fire and earth
 type WuXing struct {
