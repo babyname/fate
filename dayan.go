8f8f7fa9f94388b02967e67b793171a66a5e07ea
godcong <jumbycc@163.com>
2019-06-18 13:12:35 +0800

    update fate

--- dayan.go
+++ dayan.go
@@ -2,8 +2,7 @@ package fate
 
 //DaYan
 type DaYan struct {
-	Index   int    `bson:"index"`    //use array index
-	Fortune string `bson:"fortune"`  //吉凶
-	TianJiu string `bson:"tian_jiu"` //天九(天九地十取天九)
-	Comment string `bson:"comment"`
+	Luck    Luck   `json:"luck"`     //吉凶
+	SkyNine string `json:"sky_nine"` //天九(天九地十取天九)
+	Comment string `json:"comment"`
 }
