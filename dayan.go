82d074155ed6b7e8da7153755853ee6ff75e087d
godcong <jumbycc@163.com>
2019-06-18 13:17:15 +0800

    update fate

--- dayan.go
+++ dayan.go
@@ -1,5 +1,13 @@
 package fate
 
+var daYanList = []*DaYan{
+	{
+		//Luck:    Poin0,
+		SkyNine: "太极之数",
+		Comment: "太极之数，万物开泰，生发无穷，利禄亨通。",
+	},
+}
+
 //DaYan
 type DaYan struct {
 	Luck    Luck   `json:"luck"`     //吉凶
