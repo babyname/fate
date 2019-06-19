7bea8c960de5cc130e98baa8697affe218776926
godcong <jumbycc@163.com>
2019-06-19 13:26:05 +0800

    update fate

--- stroke.go
+++ stroke.go
@@ -1,31 +1,5 @@
 package fate
 
+//Stroke 推荐
 type Stroke struct {
-	LastStroke  []int
-	FirstStroke []int
-	wuge        *WuGe
-	sancai      *SanCai
-}
-
-//First 名字笔画
-func First(s []*Stroke, i int) []int {
-	var rlt []int
-	for idx := range s {
-		if len(s[idx].FirstStroke) > i {
-			rlt = append(rlt, s[idx].FirstStroke[i])
-		}
-
-	}
-	return rlt
-}
-
-//Last 姓氏笔画
-func Last(s []*Stroke, i int) []int {
-	var rlt []int
-	for idx := range s {
-		if len(s[idx].LastStroke) > i {
-			rlt = append(rlt, s[idx].LastStroke[i])
-		}
-	}
-	return rlt
 }
