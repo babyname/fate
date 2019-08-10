d3b5a602494ed9884a5522edc7838fe158eabe10
godcong <jumbycc@163.com>
2019-08-11 02:16:32 +0800

    update fate

--- fate_test.go
+++ fate_test.go
@@ -1,48 +1,12 @@
 package fate_test
 
 import (
-	"fmt"
-	"log"
-	"math/rand"
+	"github.com/godcong/fate"
 	"testing"
 	"time"
-
-	"github.com/godcong/fate"
 )
 
-func TestFate_EightCharacter(t *testing.T) {
-	log.SetFlags(log.Llongfile)
-	fate := fate.NewFate("毛")
-	fate.SetLunarData(time.Now())
-	//log.Println(fate.EightCharacter())
-}
-
-func TestTime(t *testing.T) {
-	tt, err := time.Parse("2006-01-02T15:04", "2017-11-14T08:17")
-	if err != nil {
-		panic(err)
-	}
-	sour := rand.NewSource(tt.UnixNano())
-	fmt.Println(rand.New(sour).Uint64())
-	fmt.Println(rand.New(sour).Uint64())
-	//10547234835636046708
-}
-
-func TestFate_BestStrokes(t *testing.T) {
-	f := fate.NewFate("毛")
-	//fate.SetMartial()
-	m := &fate.Martial{
-		BiHua:     true,
-		SanCai:    true,
-		BaZi:      false,
-		GuaXiang:  false,
-		TianYun:   false,
-		ShengXiao: false,
-	}
-
-	f.SetLunarData(time.Now())
-	g := f.Generate(6)
-	g.SetMartial(m)
-	g.Continue()
-	g.Continue()
+func TestFate_FirstRunInit(t *testing.T) {
+	newFate := fate.NewFate("李", time.Now())
+	newFate.FirstRunInit()
 }
