4880d054d9a9a93c04a5758713cd39450e8fee73
godcong <jumbycc@163.com>
2019-09-08 15:48:10 +0800

    update fate

--- database.go
+++ database.go
@@ -7,7 +7,7 @@ import (
 
 const SQLite3Source = "file:%s?cache=shared&mode=rwc&_journal_mode=WAL"
 
-func NewSqlite3(name string) (eng *xorm.Engine, e error) {
+func NewSQLite3(name string) (eng *xorm.Engine, e error) {
 	eng, e = xorm.NewEngine("sqlite3", fmt.Sprintf(SQLite3Source, name))
 	if e != nil {
 		return nil, e
