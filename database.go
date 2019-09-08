bc8c19e6a896cd66f2e4b04ed6489c2a04d6e10d
godcong <jumbycc@163.com>
2019-09-08 15:22:17 +0800

    update fate

---
+++ database.go
@@ -0,0 +1,16 @@
+package fate
+
+import (
+	"fmt"
+	"github.com/go-xorm/xorm"
+)
+
+const SQLite3Source = "file:%s?cache=shared&mode=rwc&_journal_mode=WAL"
+
+func NewSqlite3(name string) (eng *xorm.Engine, e error) {
+	eng, e = xorm.NewEngine("sqlite3", fmt.Sprintf(SQLite3Source, name))
+	if e != nil {
+		return nil, e
+	}
+	return eng, nil
+}
