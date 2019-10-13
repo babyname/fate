440b7568d133781f51e2f9b2f1b4decc0accd7d5
godcong <jumbycc@163.com>
2019-10-14 02:36:53 +0800

    update fate

--- fate_test.go
+++ fate_test.go
@@ -8,13 +8,11 @@ import (
 
 func TestFate_FirstRunInit(t *testing.T) {
 	f := fate.NewFate("张", time.Now())
-	eng, e := fate.NewSQLite3("./data/character.db")
-	if e != nil {
-		t.Fatal(e)
-	}
-	eng.ShowSQL()
-	f.SetCharDB(eng)
-	e = f.GetLastCharacter()
+	eng := fate.InitMysql("localhost:3306", "root", "111111")
+
+	f.SetDB(eng)
+	f.FirstRunInit()
+	e := f.GetLastCharacter()
 	if e != nil {
 		t.Fatal(e)
 	}
