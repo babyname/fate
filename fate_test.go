676163ee867690a3c38b10fef72874b4cec9f79c
godcong <jumbycc@163.com>
2019-11-02 02:45:44 +0800

    update fate

--- fate_test.go
+++ fate_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestFate_FirstRunInit(t *testing.T) {
 	eng := fate.InitMysql("192.168.1.161:3306", "root", "111111")
-	c := chronos.New("2019/10/31 11:31")
+	c := chronos.New("2020/01/31 11:31")
 	//t.Log(c.Solar().Time())
 	f := fate.NewFate("王", c.Solar().Time(), fate.Database(eng))
 
