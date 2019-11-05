e8afeb1804b9fcf0d4329dc5a1f99f4b65af5088
godcong <jumbycc@163.com>
2019-11-05 16:49:08 +0800

    update fate

--- cmd/console/main.go
+++ cmd/console/main.go
@@ -2,43 +2,23 @@ package main
 
 import (
 	"log"
-	"os"
 	"runtime"
 	"time"
-
-	"github.com/godcong/fate/config"
-	"github.com/godcong/fate/mongo"
-	"github.com/globalsign/mgo"
-	"github.com/godcong/fate"
 )
 
 func main() {
-	args := os.Args
-	if len(args) > 0 {
-		switch args[0] {
-		case "init":
-			fallthrough
-		default:
-			initSCWG()
-		}
-	}
+
 }
 
 func initSCWG() {
 	max := runtime.NumCPU() * 2
 	runtime.GOMAXPROCS(max)
 	l1, l2, f1, f2 := 1, 0, 1, 0
-	mongo.Dial(config.Default().GetStringD("mongodb.url", "localhost"),
-		&mgo.Credential{
-			Username: config.Default().GetStringD("mongodb.username", "root"),
-			Password: config.Default().GetStringD("mongodb.password", "root"),
-		})
 
 	ch := make(chan bool, max)
 
 	for i := 0; i < max; i++ {
 		go runtineProcess(ch, l1, l2, f1, f2)
-		l1, l2, f1, f2 = valuePlus(l1, l2, f1, f2)
 	}
 
 	for {
@@ -49,7 +29,6 @@ func initSCWG() {
 					log.Println("process:", l1, l2, f1, f2, time.Now())
 				}
 				go runtineProcess(ch, l1, l2, f1, f2)
-				l1, l2, f1, f2 = valuePlus(l1, l2, f1, f2)
 			}
 		case <-time.After(10 * time.Second):
 			log.Println("time out")
@@ -60,25 +39,5 @@ func initSCWG() {
 }
 
 func runtineProcess(b chan<- bool, l1, l2, f1, f2 int) {
-	wuGe := fate.MakeWuGe(l1, l2, f1, f2)
-	sanCai := fate.NewSanCai(wuGe)
-	mongo.InsertIfNotExist(mongo.C("sancai"), sanCai)
-	b <- true
-}
 
-func valuePlus(l1, l2, f1, f2 int) (int, int, int, int) {
-	f2++
-	if f2 > 32 {
-		f2 = 0
-		f1++
-	}
-	if f1 > 32 {
-		f1 = 1
-		l2++
-	}
-	if l2 > 32 {
-		l2 = 0
-		l1++
-	}
-	return l1, l2, f1, f2
 }
