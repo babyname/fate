19aefe87916641e05ca0c7e912176079da948c7a
godcong <jumbycc@163.com>
2019-11-10 21:39:05 +0800

    update fate

--- cmd/console/main.go
+++ cmd/console/main.go
@@ -1,43 +1,30 @@
 package main
 
 import (
-	"log"
-	"runtime"
-	"time"
+	"github.com/urfave/cli/v2"
 )
+
+const programName = `fate`
 
 func main() {
 
-}
-
-func initSCWG() {
-	max := runtime.NumCPU() * 2
-	runtime.GOMAXPROCS(max)
-	l1, l2, f1, f2 := 1, 0, 1, 0
-
-	ch := make(chan bool, max)
-
-	for i := 0; i < max; i++ {
-		go runtineProcess(ch, l1, l2, f1, f2)
+	app := cli.NewApp()
+	app.Name = programName
+	app.Usage = "CLI for IPFS Cluster"
+	app.Flags = []cli.Flag{
+		&cli.StringFlag{
+			Name:  "last, l",
+			Value: "",
+			Usage: "set the last name",
+		},
+		&cli.StringFlag{
+			Name:  "born",
+			Value: "",
+			Usage: "set the born date Format(2006/01/02 03:04)",
+		},
 	}
 
-	for {
-		select {
-		case <-ch:
-			if l1 <= 32 {
-				if f1 == 1 && f2 == 0 {
-					log.Println("process:", l1, l2, f1, f2, time.Now())
-				}
-				go runtineProcess(ch, l1, l2, f1, f2)
-			}
-		case <-time.After(10 * time.Second):
-			log.Println("time out")
-			break
-		}
+	app.Before = func(c *cli.Context) error {
+		return nil
 	}
-
-}
-
-func runtineProcess(b chan<- bool, l1, l2, f1, f2 int) {
-
 }
