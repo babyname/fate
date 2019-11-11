8baff2c89a8024dba328d401106b0890513fa285
godcong <jumbycc@163.com>
2019-11-12 01:47:44 +0800

    update fate

--- cmd/console/main.go
+++ cmd/console/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"github.com/godcong/chronos"
+	"github.com/godcong/fate"
 	"github.com/urfave/cli/v2"
+	"os"
 )
 
 const programName = `fate`
@@ -22,15 +25,32 @@ func main() {
 			Value: "",
 			Usage: "set the born date Format(2006/01/02 03:04)",
 		},
-		&cli.StringFlag{
-			Name:    "database, db",
-			Aliases: nil,
-			Usage:   "set the database address",
-			Value:   "",
+		&cli.StringSliceFlag{
+			Name:  "database, db",
+			Usage: "set the database address",
+			Value: cli.NewStringSlice("localhost:3306", "root", "111111"),
 		},
 	}
-
+	var f fate.Fate
 	app.Before = func(c *cli.Context) error {
+		db := c.StringSlice("database")
+		eng := fate.InitMysql(db[0], db[1], db[2])
+		chronos := chronos.New(c.String("born"))
+		//fate.DefaultStrokeMin = 3
+		//fate.DefaultStrokeMax = 10
+		//fate.HardMode = false
+		f = fate.NewFate(c.String("last"), chronos.Solar().Time(), fate.Database(eng), fate.BaGuaFilter(), fate.ZodiacFilter(), fate.SupplyFilter())
 		return nil
+	}
+	app.Action = func(context *cli.Context) error {
+		e := f.MakeName()
+		if e != nil {
+			return e
+		}
+		return nil
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		panic(err)
 	}
 }
