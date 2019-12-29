62bc3c02747218ab46bb59b6a523e121137dbe0f
godcong <jumbycc@163.com>
2019-12-29 23:14:37 +0800

    update fix

--- cmd/strokefix/check.go
+++ cmd/strokefix/check.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"github.com/godcong/fate"
+	"github.com/goextension/log"
+	"github.com/xormsharp/xorm"
 	"golang.org/x/xerrors"
 	"io/ioutil"
 )
@@ -24,4 +27,21 @@ func CheckLoader(s string) error {
 	}
 
 	return nil
+}
+
+func CheckVerify(db *xorm.Engine) error {
+	for idx, s := range checks {
+		log.Infow("check", "character", s)
+		character, e := fate.GetCharacter(db, func(eng *xorm.Engine) *xorm.Session {
+			return eng.Where("ch = ?", s)
+		})
+		if e != nil {
+			log.Errorw("check error", "character", s)
+			continue
+		}
+		if character.ScienceStroke != idx {
+			log.Warnw("check warning", "character", s, "db", character.ScienceStroke, "need", idx)
+		}
+	}
+
 }
