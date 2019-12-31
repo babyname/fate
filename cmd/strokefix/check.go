1e61b9addfc55e4a42c3e1fd17e3112094f2d0a6
godcong <jumbycc@163.com>
2020-01-01 01:12:38 +0800

    update fix

--- cmd/strokefix/check.go
+++ cmd/strokefix/check.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"github.com/godcong/fate"
 	"github.com/goextension/log"
 	"github.com/xormsharp/xorm"
-	"golang.org/x/xerrors"
 	"io/ioutil"
+	"strconv"
 )
 
-var checks map[int]string
+type Dict struct {
+	Jin  map[string][]string `json:"jin"`
+	Mu   map[string][]string `json:"mu"`
+	Huo  map[string][]string `json:"huo"`
+	Shui map[string][]string `json:"shui"`
+	Tu   map[string][]string `json:"tu"`
+}
 
 func CheckLoader(s string) error {
 	bytes, e := ioutil.ReadFile(s)
@@ -23,24 +30,32 @@ func CheckLoader(s string) error {
 	}
 
 	if len(checks) == 0 {
-		return xerrors.New("no data")
+		return errors.New("no data")
 	}
 
 	return nil
 }
 
 func CheckVerify(db *xorm.Engine) error {
-	for idx, s := range checks {
-		log.Infow("check", "character", s)
-		character, e := fate.GetCharacter(db, func(eng *xorm.Engine) *xorm.Session {
-			return eng.Where("ch = ?", s)
-		})
-		if e != nil {
-			log.Errorw("check error", "character", s)
-			return e
-		}
-		if character.ScienceStroke != idx {
-			log.Warnw("check warning", "character", s, "db", character.ScienceStroke, "need", idx)
+	for _, s := range checks {
+		for five, val := range s {
+			for idx, vv := range val {
+				log.Infof("%+v,val:%+v", idx, val)
+				continue
+				log.Infow("check", "character", s)
+				character, e := fate.GetCharacter(db, func(eng *xorm.Engine) *xorm.Session {
+					return eng.Where("ch = ?", s)
+				})
+				if e != nil {
+					log.Errorw("check error", "character", s)
+					return e
+				}
+				i, _ := strconv.Atoi(idx)
+				if character.ScienceStroke != i {
+					log.Warnw("check warning", "character", s, "db", character.ScienceStroke, "need", idx)
+				}
+			}
+
 		}
 	}
 	return nil
