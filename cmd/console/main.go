e3888122b95363cb04eb37e88165c872ffbd8937
godcong <jumbycc@163.com>
2020-01-03 00:45:01 +0800

    update science stroke

--- cmd/console/main.go
+++ cmd/console/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import "fmt"
+import "github.com/spf13/cobra"
 
+const programName = `fate`
+
+var rootCmd = &cobra.Command{
+	Use:     "fate [command]",
+	Short:   "call fate command",
+	Version: "v0.0.1",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("arguments [command] was not inputted")
+	},
+	DisableSuggestions:         false,
+	SuggestionsMinimumDistance: 1,
+}
+
+func main() {
+	rootCmd.AddCommand(cmdInit())
+	e := rootCmd.Execute()
+	if e != nil {
+		panic(e)
+	}
 }
