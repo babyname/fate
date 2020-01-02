b7d96c8352b2249051bce29cc718800dc25c2622
godcong <jumbycc@163.com>
2020-01-03 01:05:37 +0800

    add console

--- config/database.go
+++ config/database.go
@@ -1,14 +1,20 @@
 package config
 
 type Database struct {
-	Host       string `json:"host"`
-	Port       string `json:"port"`
-	User       string `json:"user"`
-	Pwd        string `json:"pwd"`
-	Name       string `json:"name"`
-	MaxIdleCon int    `json:"max_idle_con"`
-	MaxOpenCon int    `json:"max_open_con"`
-	Driver     string `json:"driver"`
-	File       string `json:"file"`
-	Dsn        string `json:"dsn"`
+	Host         string `json:"host"`
+	Port         string `json:"port"`
+	User         string `json:"user"`
+	Pwd          string `json:"pwd"`
+	Name         string `json:"name"`
+	MaxIdleCon   int    `json:"max_idle_con"`
+	MaxOpenCon   int    `json:"max_open_con"`
+	Driver       string `json:"driver"`
+	File         string `json:"file"`
+	Dsn          string `json:"dsn"`
+	ShowSQL      bool   `json:"show_sql"`
+	ShowExecTime bool   `json:"show_exec_time"`
+}
+
+func (d *Database) Addr() string {
+	return d.Host + ":" + d.Port
 }
