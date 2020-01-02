9c6eda208f8164de23c4a4f4beaff8215ed49fac
godcong <jumbycc@163.com>
2020-01-03 01:05:37 +0800

    add console

--- database.go
+++ database.go
@@ -3,6 +3,7 @@ package fate
 import (
 	"fmt"
 	"github.com/go-sql-driver/mysql"
+	"github.com/godcong/fate/config"
 	"github.com/goextension/log"
 
 	"github.com/xormsharp/xorm"
@@ -15,26 +16,45 @@ const createDatabase = "CREATE DATABASE `%s` CHARACTER SET 'utf8mb4' COLLATE 'ut
 var _ = mysql.Config{}
 var DefaultTableName = "fate"
 
-func InitMysql(addr, name, pass string) *xorm.Engine {
+type Database interface {
+}
 
-	dbURL := fmt.Sprintf(mysqlSource, name, pass, addr, "", url.QueryEscape("Asia/Shanghai"))
-	dbEngine, e := xorm.NewEngine("mysql", dbURL)
+type xormDatabase struct {
+	*xorm.Engine
+}
+
+func InitFromConfig(config config.Config) Database {
+	engine := initSQL(config.Database)
+	return xormDatabase{
+		Engine: engine,
+	}
+}
+
+func initSQL(database config.Database) *xorm.Engine {
+	dbURL := fmt.Sprintf(mysqlSource, database.User, database.Pwd, database.Addr(), "", url.QueryEscape("Asia/Shanghai"))
+	dbEngine, e := xorm.NewEngine(database.Driver, dbURL)
 	if e != nil {
 		log.Panicw("connect database failed", "error", e)
 	}
 	defer dbEngine.Close()
-	sql := fmt.Sprintf(createDatabase, DefaultTableName)
+
+	sql := fmt.Sprintf(createDatabase, database.Name)
 
 	_, e = dbEngine.DB().Exec(sql)
 	if e == nil {
 		log.Infow("create database failed", "database", DefaultTableName)
 	}
-	u := fmt.Sprintf(mysqlSource, name, pass, addr, DefaultTableName, url.QueryEscape("Asia/Shanghai"))
+	u := fmt.Sprintf(mysqlSource, database.User, database.Pwd, database.Addr(), database.Name, url.QueryEscape("Asia/Shanghai"))
 	eng, e := xorm.NewEngine("mysql", u)
 	if e != nil {
 		log.Panicw("connect table failed", "error", e)
 	}
-	eng.ShowSQL(true)
-	eng.ShowExecTime(true)
+	if database.ShowSQL {
+		eng.ShowSQL(true)
+	}
+
+	if database.ShowExecTime {
+		eng.ShowExecTime(true)
+	}
 	return eng
 }
