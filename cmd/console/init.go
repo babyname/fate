ae4277ae312baa7ec6afbb66c062699e0f32742e
godcong <jumbycc@163.com>
2020-01-04 23:18:13 +0800

    update fate

--- cmd/console/init.go
+++ cmd/console/init.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/godcong/fate/config"
 	"github.com/spf13/cobra"
 )
 
@@ -12,6 +13,10 @@ func cmdInit() *cobra.Command {
 		Short: "output the init config",
 		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Printf("config will output to %s/config.json\n", path)
+			e := config.OutputConfig(path, config.DefaultConfig())
+			if e != nil {
+				panic(e)
+			}
 		},
 	}
 	cmd.Flags().StringVarP(&path, "path", "p", ".", "set the output path")
