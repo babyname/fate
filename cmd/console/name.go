9012ca218853a4101eeeba75b403ccf5c2602f77
godcong <jumbycc@163.com>
2020-01-06 01:00:09 +0800

    update fate

--- cmd/console/name.go
+++ cmd/console/name.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
+	"github.com/godcong/chronos"
+	"github.com/godcong/fate"
 	"github.com/godcong/fate/config"
 	"github.com/spf13/cobra"
+	"time"
 )
 
 func cmdName() *cobra.Command {
 	var path string
+	var born string
+	var last string
 	cmd := &cobra.Command{
 		Use:   "name",
 		Short: "output the name",
@@ -16,8 +21,16 @@ func cmdName() *cobra.Command {
 			config.DefaultJSONPath = path
 			cfg := config.LoadConfig()
 			fmt.Printf("config loaded: %+v", cfg)
+			bornTime, e := time.Parse(chronos.DateFormat, born)
+			if e != nil {
+				return
+			}
+			f := fate.NewFate(last, bornTime)
+
 		},
 	}
+	cmd.Flags().StringVarP(&last, "last", "l", "", "set lastname")
+	cmd.Flags().StringVarP(&born, "born", "b", time.Now().Format(chronos.DateFormat), "set birth as 2016/01/02 15:04")
 	cmd.Flags().StringVarP(&path, "path", "p", ".", "set the input path")
 	return cmd
 }
