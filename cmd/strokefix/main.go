ae97bd6c65aa641ad10485627e07606fcbdc5656
godcong <jumbycc@163.com>
2020-01-14 00:12:25 +0800

    update fate

--- cmd/strokefix/main.go
+++ cmd/strokefix/main.go
@@ -1,13 +1,19 @@
 package main
 
-import "github.com/godcong/fate"
+import (
+	"github.com/godcong/fate"
+	"github.com/godcong/fate/config"
+)
 
 func main() {
 	var e error
-	db := fate.InitMysql("127.0.0.1:3306", "root", "111111")
-	e = db.Sync2(fate.Character{})
+
+	cfg := config.DefaultConfig()
+	db := fate.InitDatabaseFromConfig(*cfg)
+
+	e = db.Sync(fate.Character{})
 	if e != nil {
-		panic(e)
+		return
 	}
 
 	//e = UpdateFix(db)
