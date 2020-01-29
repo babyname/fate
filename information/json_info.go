a45978d389df0895aff12bb799e49662018d00f9
godcong <jumbycc@163.com>
2020-01-29 22:37:48 +0800

    update fate

--- information/json_info.go
+++ information/json_info.go
@@ -1,4 +1,19 @@
 package information
 
+import "os"
+
 type jsonInformation struct {
+	path string
+	file *os.File
+}
+
+func jsonOutput(path string) Information {
+	file, e := os.OpenFile(path, os.O_CREATE|os.O_APPEND|os.O_SYNC|os.O_RDWR, 0755)
+	if e != nil {
+		return nil
+	}
+	return &jsonInformation{
+		path: path,
+		file: file,
+	}
 }
