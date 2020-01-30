978ce1b7b439a70806a180ef2b90c8debf658ef5
godcong <jumbycc@163.com>
2020-01-30 17:15:07 +0800

    update fate

--- information.go
+++ information.go
@@ -1,13 +1,26 @@
-package information
+package fate
 
 import (
 	"fmt"
+	"github.com/godcong/fate/config"
 	"os"
 )
+
+type Information interface {
+	Write(src string)
+}
+
+func NewWithConfig(config config.Config) Information {
+	return nil
+}
 
 type jsonInformation struct {
 	path string
 	file *os.File
+}
+
+func (j jsonInformation) Write(src string) {
+	panic("implement me")
 }
 
 func jsonOutput(path string) Information {
