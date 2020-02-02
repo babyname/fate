daf76087dfeadff807780341c2b9612c2c1d0a41
godcong <jumbycc@163.com>
2020-02-03 02:34:27 +0800

    update fate

--- log.go
+++ log.go
@@ -1,46 +1,9 @@
 package fate
 
 import (
-	"github.com/goextension/log"
 	zap2 "github.com/goextension/log/zap"
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 )
 
 func init() {
-	cfg := zap.NewProductionConfig()
-
-	cfg.EncoderConfig = zapcore.EncoderConfig{
-		MessageKey:     "msg",
-		LevelKey:       "",
-		TimeKey:        "",
-		NameKey:        "",
-		CallerKey:      "",
-		StacktraceKey:  "",
-		LineEnding:     "",
-		EncodeLevel:    nil,
-		EncodeTime:     nil,
-		EncodeDuration: nil,
-		EncodeCaller:   nil,
-		EncodeName:     nil,
-	}
-	cfg.OutputPaths = []string{
-		"name.txt",
-	}
-
-	cfg.ErrorOutputPaths = []string{
-		zap2.DefaultZapFilePath,
-	}
-
-	cfg.DisableCaller = true
-	cfg.DisableStacktrace = true
-
-	logger, e := cfg.Build(
-		zap.AddCaller(),
-		zap.AddCallerSkip(1),
-	)
-	if e != nil {
-		panic(e)
-	}
-	log.Register(logger.Sugar())
+	zap2.InitZapFileSugar()
 }
