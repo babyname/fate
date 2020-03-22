f3ff41d874889fd77503454bddcff5d59f9baa98
godcong <jumbycc@163.com>
2020-03-23 00:00:54 +0800

    update fate

---
+++ regular/regular_test.go
@@ -0,0 +1,14 @@
+package regular
+
+import (
+	"github.com/godcong/fate"
+	"github.com/godcong/fate/config"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	c := config.LoadConfig()
+	db := fate.InitDatabaseWithConfig(*c)
+	regular := New(db)
+	regular.Run()
+}
