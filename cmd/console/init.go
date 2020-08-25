7cd89c3bfb7d484bf2acc89bd9c717fbecb28970
godcong <jumbycc@163.com>
2020-08-25 10:29:04 +0800

    update fate

--- cmd/console/init.go
+++ cmd/console/init.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"os"
-	"path/filepath"
-	"runtime"
-
 	"github.com/godcong/fate/config"
 	"github.com/goextension/log"
 	"github.com/spf13/cobra"
+	"path/filepath"
 )
 
 func cmdInit() *cobra.Command {
@@ -34,24 +30,4 @@ func cmdInit() *cobra.Command {
 	}
 	cmd.Flags().StringVarP(&path, "path", "p", "", "set the output path")
 	return cmd
-}
-
-func zoneCheck() error {
-	log.Info("GOROOT:", runtime.GOROOT())
-	path := runtime.GOROOT() + "/lib/time"
-	_, e := os.Stat(filepath.Join(path, "zoneinfo.zip"))
-	if e != nil && os.IsNotExist(e) {
-		_, e1 := os.Stat(filepath.Join(getCurrentPath(), "zoneinfo.zip"))
-		if e1 != nil && os.IsNotExist(e) {
-			return errors.New("zoneinfo file not found")
-		} else if e1 != nil {
-			return fmt.Errorf("found error in current path:%w", e1)
-		} else {
-			return nil
-		}
-	} else if e != nil {
-		return fmt.Errorf("found error in go path:%w", e)
-	} else {
-		return nil
-	}
 }
