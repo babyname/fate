87ffb3d87955c06835b53cb78f8ea8b3bfc2897b
godcong <jumbycc@163.com>
2021-08-09 21:55:05 +0800

    update database to ent

--- model/character.go
+++ model/character.go
@@ -6,6 +6,20 @@ import (
 	"github.com/godcong/fate/ent"
 )
 
-func (m Model) GetCharacter(ctx context.Context, char string) (*ent.Character, error) {
-	return m.Character.Query().First(ctx)
+func (m Model) GetCharacter(ctx context.Context, char string, filters ...func(query *ent.CharacterQuery) *ent.CharacterQuery) (
+	*ent.Character, error) {
+	q := m.Character.Query()
+	for _, filter := range filters {
+		q = filter(q)
+	}
+	return q.First(ctx)
+}
+
+func (m Model) GetCharacters(ctx context.Context, filters ...func(query *ent.CharacterQuery) *ent.CharacterQuery) (
+	[]*ent.Character, error) {
+	q := m.Character.Query()
+	for _, filter := range filters {
+		q = filter(q)
+	}
+	return q.All(ctx)
 }
