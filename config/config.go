6c36e4bb0eaa095bb43bfa62070def3d96fd7c31
godcong <jumbycc@163.com>
2021-09-16 15:10:25 +0800

    update database to ent

--- config/config.go
+++ config/config.go
@@ -34,18 +34,11 @@ type OutputFormat struct {
 }
 
 type Config struct {
-	FilterMode   FilterMode   `json:"filter_mode"`
-	StrokeMax    int          `json:"stroke_max"`
-	StrokeMin    int          `json:"stroke_min"`
-	FixBazi      bool         `json:"fix_bazi"`      //八字修正
-	SupplyFilter bool         `json:"supply_filter"` //过滤补八字
-	ZodiacFilter bool         `json:"zodiac_filter"` //过滤生肖
-	BaguaFilter  bool         `json:"bagua_filter"`  //过滤卦象
-	DayanFilter  bool         `json:"dayan_filter"`  //过滤大衍
-	Regular      bool         `json:"regular"`       //常用字过滤
-	Database     DBConfig     `json:"database"`
-	FileOutput   OutputFormat `json:"file_output"`
-	Debug        bool
+	StrokeMax int          `json:"stroke_max"`
+	StrokeMin int          `json:"stroke_min"`
+	Database  DBConfig     `json:"database"`
+	Output    OutputFormat `json:"output"`
+	Debug     bool         `json:"debug"`
 }
 
 var DefaultJSONPath = ""
@@ -91,22 +84,15 @@ func OutputConfig(config *Config) error {
 
 func DefaultConfig() *Config {
 	return &Config{
-		Debug:        false,
-		FilterMode:   0,
-		StrokeMax:    18,
-		StrokeMin:    3,
-		DayanFilter:  false,
-		FixBazi:      false,
-		SupplyFilter: true,
-		ZodiacFilter: true,
-		BaguaFilter:  true,
-		Regular:      true,
+		Debug:     false,
+		StrokeMax: 18,
+		StrokeMin: 3,
 		Database: DBConfig{
 			Driver: "mysql",
 			DSN:    "root:111111@tcp(192.168.2.201:3306)/fate?charset=utf8\\u0026parseTime=true",
 			Log:    "",
 		},
-		FileOutput: OutputFormat{
+		Output: OutputFormat{
 			Heads:      DefaultHeads,
 			OutputMode: OutputModeLog,
 			Path:       "name.txt",
