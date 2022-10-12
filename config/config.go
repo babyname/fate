154eea0eada56742d8895c422030ca832ac983e7
godcong <jumbycc@163.com>
2022-10-12 22:26:32 +0800

    chore(fate): update config

--- config/config.go
+++ config/config.go
@@ -6,31 +6,48 @@ import (
 	"github.com/tikafog/jsongs"
 )
 
-type Config struct {
-	Database database `json:"database"`
+type Config interface {
+	Database() Database
+	Save(path string) error
+}
+
+type config struct {
+	database database `json:"database" json-getter:"GetDatabase"`
 	Debug    bool     `json:"debug"`
 }
 
-func (c Config) GetDatabase() Database {
-	return &c.Database
+func (c *config) Save(path string) error {
+	return saveConfig(path, c)
 }
 
-func LoadConfig(path string) (c *Config) {
-	c = &Config{}
-	def := DefaultConfig()
+func (c *config) SetDatabase(database database) {
+	c.database = database
+}
+
+func (c *config) Database() Database {
+	return &c.database
+}
+
+func (c *config) GetDatabase() database {
+	return c.database
+}
+
+func LoadConfig(path string) (c Config) {
+	c = &config{}
+	d := DefaultConfig()
 
 	bys, e := os.ReadFile(path)
 	if e != nil {
-		return def
+		return d
 	}
 	e = jsongs.Unmarshal(bys, &c)
 	if e != nil {
-		return def
+		return d
 	}
 	return c
 }
 
-func SaveConfig(path string, config *Config) error {
+func saveConfig(path string, config *config) error {
 	bytes, e := jsongs.MarshalIndent(config, "", " ")
 	if e != nil {
 		return e
@@ -38,10 +55,10 @@ func SaveConfig(path string, config *Config) error {
 	return os.WriteFile(path, bytes, 0644)
 }
 
-func DefaultConfig() *Config {
-	return &Config{
+func DefaultConfig() Config {
+	return &config{
 		Debug: false,
-		Database: database{
+		database: database{
 			driver: "mysql",
 			dsn:    mysqlDSN,
 			host:   "localhost",
@@ -53,10 +70,10 @@ func DefaultConfig() *Config {
 	}
 }
 
-func DefaultSqliteConfig() *Config {
-	return &Config{
+func DefaultSqliteConfig() Config {
+	return &config{
 		Debug: false,
-		Database: database{
+		database: database{
 			driver: "sqlite3",
 			dsn:    sqlite3DSN,
 			dbName: "fate",
