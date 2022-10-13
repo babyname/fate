34eedd63eeac405b5b9ed50259678ed80526ba9e
godcong <jumbycc@163.com>
2022-10-13 15:15:06 +0800

    feat(fate): update changed

--- model/model.go
+++ model/model.go
@@ -4,36 +4,11 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 
-	"github.com/goextension/log"
-
-	"github.com/babyname/fate/config"
 	"github.com/babyname/fate/ent"
 )
 
-const dsn = "%s:%s@tcp(%s)/%s?loc=%s&charset=utf8mb4&parseTime=true"
-
 type Model struct {
 	*ent.Client
-	cfg config.Database
-}
-
-func Open(cfg config.Database, debug bool) (*Model, error) {
-	var options []ent.Option
-	if debug {
-		options = append(options, ent.Debug())
-		options = append(options, ent.Log(func(i ...interface{}) {
-			log.Debug(i...)
-		}))
-	}
-
-	open, err := ent.Open(cfg.Driver, cfg.DSN, options...)
-	if err != nil {
-		return nil, err
-	}
-	return &Model{
-		Client: open,
-		cfg:    cfg,
-	}, nil
 }
 
 // ID ...
