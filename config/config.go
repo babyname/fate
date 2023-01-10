e3cf4088354a18a41a9ea34734e3932e64995bff
godcong <jumbycc@163.com>
2023-01-10 21:05:23 +0800

    feat(fate): change for v4

--- config/config.go
+++ config/config.go
@@ -31,41 +31,34 @@ type FileOutput struct {
 }
 
 type Config struct {
-	RunInit      bool       `json:"run_init"`
-	FilterMode   FilterMode `json:"filter_mode"`
-	StrokeMax    int        `json:"stroke_max"`
-	StrokeMin    int        `json:"stroke_min"`
-	HardFilter   bool       `json:"hard_filter"`
-	FixBazi      bool       `json:"fix_bazi"`      //八字修正
-	SupplyFilter bool       `json:"supply_filter"` //过滤补八字
-	ZodiacFilter bool       `json:"zodiac_filter"` //过滤生肖
-	BaguaFilter  bool       `json:"bagua_filter"`  //过滤卦象
-	Regular      bool       `json:"regular"`       //常用
-	Database     DBConfig   `json:"database"`
-	FileOutput   FileOutput `json:"file_output"`
+
+	//RunInit      bool       `json:"run_init"`
+	//FilterMode   FilterMode `json:"filter_mode"`
+	//StrokeMax    int        `json:"stroke_max"`
+	//StrokeMin    int        `json:"stroke_min"`
+	//HardFilter   bool       `json:"hard_filter"`
+	//FixBazi      bool       `json:"fix_bazi"`      //八字修正
+	//SupplyFilter bool       `json:"supply_filter"` //过滤补八字
+	//ZodiacFilter bool       `json:"zodiac_filter"` //过滤生肖
+	//BaguaFilter  bool       `json:"bagua_filter"`  //过滤卦象
+	//Regular      bool       `json:"regular"`       //常用
+	//FileOutput   FileOutput `json:"file_output"`
+	WorkingDir string    `json:"working_dir,omitempty"`
+	Database   DBConfig  `json:"database"`
+	Log        LogConfig `json:"logger,omitempty"`
 }
 
-var DefaultJSONPath = ""
+const (
+	defaultConfigPath = ""
+	defaultWorkingDir = "fate"
+)
+
 var DefaultHeads = []string{"姓名", "笔画", "拼音", "喜用神", "八字"}
 
-func init() {
-	if DefaultJSONPath == "" {
-		dir, err := os.Getwd()
-		if err != nil {
-			panic(err)
-		}
-		s, err := filepath.Abs(dir)
-		if err != nil {
-			panic(err)
-		}
-		DefaultJSONPath = s
-	}
-}
-
-func LoadConfig() (c *Config) {
+func LoadConfig(path string) (c *Config) {
 	c = &Config{}
 	def := DefaultConfig()
-	f := filepath.Join(DefaultJSONPath, JSONName)
+	f := filepath.Join(defaultConfigPath, JSONName)
 	bys, e := os.ReadFile(f)
 	if e != nil {
 		return def
@@ -83,26 +76,30 @@ func OutputConfig(config *Config) error {
 		return e
 	}
 
-	return os.WriteFile(filepath.Join(DefaultJSONPath, JSONName), bys, 0755)
+	return os.WriteFile(filepath.Join(defaultConfigPath, JSONName), bys, 0755)
 }
 
 func DefaultConfig() *Config {
 	return &Config{
-		RunInit:      false,
-		FilterMode:   0,
-		StrokeMax:    18,
-		StrokeMin:    3,
-		HardFilter:   false,
-		FixBazi:      false,
-		SupplyFilter: true,
-		ZodiacFilter: true,
-		BaguaFilter:  true,
-		Regular:      true,
-		Database:     defaultSqlite3(),
-		FileOutput: FileOutput{
-			Heads:      DefaultHeads,
-			OutputMode: OutputModeLog,
-			Path:       "name.txt",
-		},
+		WorkingDir: defaultWorkingDir,
+		Database:   defaultDBSqlite3(),
+		Log:        defaultLogConfig(),
 	}
+}
+
+func GetPath(root string, paths ...string) string {
+	workpath := filepath.Join(paths...)
+	path := filepath.Join(defaultWorkingDir, workpath)
+	if root == "" {
+		dir, err := os.Getwd()
+		if err != nil {
+			return path
+		}
+		s, err := filepath.Abs(dir)
+		if err != nil {
+			return path
+		}
+		return s
+	}
+	return filepath.Join(root, workpath)
 }
