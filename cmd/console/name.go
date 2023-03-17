c45d928e55a75ca015c39cf85c3d9c01837770e9
godcong <jumbycc@163.com>
2023-03-17 15:32:21 +0800

    feat(fate): change for v4

--- cmd/console/name.go
+++ cmd/console/name.go
@@ -15,6 +15,7 @@ func cmdName() *cobra.Command {
 	var born string
 	var last string
 	var output string
+	var filter string
 	var sex string
 	cmd := &cobra.Command{
 		Use:   "name",
@@ -69,22 +70,36 @@ func cmdName() *cobra.Command {
 				return
 			}
 			<-s.Context().Done()
-			fmt.Println("end", time.Now().String())
+			fmt.Println("query end", time.Now().String())
 			time.Sleep(3 * time.Second)
-
-			for name, ok := input.Output().NextName(); ok; name, ok = input.Output().NextName() {
-				fmt.Print("Name:")
-				for j := 0; j < 10; j++ {
-					fmt.Print(name, "  |  ")
+			if len(filter) > 0 {
+				fr := []rune(filter)
+				for i := range fr {
+					filters := input.Output().Filter(string(fr[i]))
+					fmt.Println("文字", string(fr[i]), "过滤了", filters, "个")
 				}
-				fmt.Printf("\r\n")
 			}
+			fmt.Println("end", time.Now().String())
+			//OutputName:
+			//for name, ok := input.Output().NextName(); ; {
+			//	fmt.Print("Name:")
+			//	for j := 0; j < 10; j++ {
+			//		if !ok {
+			//			break OutputName
+			//		}
+			//		fmt.Print(name, "  |  ")
+			//		name, ok = input.Output().NextName()
+			//	}
+			//	fmt.Printf("\r\n")
+			//}
+			//fmt.Printf("\r\n")
 			fmt.Println("Total", input.Output().Total())
 		},
 	}
 	cmd.Flags().StringVarP(&last, "last", "l", "", "指定姓氏")
 	cmd.Flags().StringVarP(&born, "born", "b", time.Now().Format(chronos.DateFormat), "设置新生儿生日 2016/01/02 15:04")
 	cmd.Flags().StringVarP(&sex, "sex", "s", "boy", "设置新生儿性别")
+	cmd.Flags().StringVarP(&filter, "filter", "f", "", "从结果中过滤掉指定汉字")
 	cmd.Flags().StringVarP(&output, "output", "o", "", "设置输出路径")
 	return cmd
 }
