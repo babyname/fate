ec05377e20984188a8e2de38254181870c9e72cd
godcong <jumbycc@163.com>
2026-05-26 16:55:14 +0800

    refactor(deps): align chronos/yi imports with actual repository paths, remove replace directives

--- fate_analysis_test.go
+++ fate_analysis_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 	"time"
 
-	v2 "github.com/babyname/chronos/v2"
+	v2 "github.com/godcong/chronos/v2"
 	"github.com/babyname/fate/config"
 	"github.com/babyname/fate/ent"
 	"github.com/babyname/fate/internal/analysis"
--- internal/naming/naming.go
+++ internal/naming/naming.go
@@ -4,7 +4,7 @@ package naming
 import (
 	"context"
 
-	v2 "github.com/babyname/chronos/v2"
+	v2 "github.com/godcong/chronos/v2"
 	"github.com/babyname/fate/config"
 	"github.com/babyname/fate/ent"
 	"github.com/babyname/fate/internal/repository"
